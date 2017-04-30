d06eb3aea2507708e99ca42539a4c70af53739b0
xtt <meteorologytoday@gmail.com>
2017-04-30 06:19:55 -0400

    fix previous commit's bug

--- src/TWSEStockDownloader.py
+++ src/TWSEStockDownloader.py
@@ -117,12 +117,12 @@ class TWSEStockDownloader(StockDownloader):
 								print("查無資料，跳過！")
 								err.append('empty')
 								break
-	
-							no_vol = False
-							# 該日無成交
-							if row['vol'] == '0':
-								print("%s 無成交量！" % (row['date'],))
-								no_vol = True
+		
+							fixed = False
+							# 該日無漲跌
+							if row['o_p'] == '0.00':
+								print("%s 無價格波動！" % (row['date'],))
+								fixed = True
 
 							tmp = row['date'][0:9].split('/')
 							row['date'] = int(datetime.datetime(int(tmp[0])+1911, int(tmp[1]), int(tmp[2]), tzinfo=datetime.timezone.utc).timestamp())
@@ -130,7 +130,7 @@ class TWSEStockDownloader(StockDownloader):
 							row['no'] = stockno	
 							row['change_spread'] = row['change_spread'].replace('X', '')
 							
-							if no_vol:
+							if fixed:
 								for key in strip:
 									row[key] = None
 							else:
