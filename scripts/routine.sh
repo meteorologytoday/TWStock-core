2c9fcfe04e15ebf559f3d85d49c5efca72dae682
meteorologytoday <meteorologytoday@github.com>
2017-05-12 01:07:44 +0800

    Fix bugs

--- scripts/routine.sh
+++ scripts/routine.sh
@@ -4,5 +4,5 @@ src_path=$TWSEStockPATH/src
 echo "Download Months: 2"
 $scripts_path/download_Stock.sh 2
 $scripts_path/download_BizCorp.sh 2
-$src_path/reorder_data.py --database=STOCK.db
+python3 $src_path/reorder_data.py --database=STOCK.db
 echo "Download is finished!"
