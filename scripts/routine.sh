a9b9fda4d8ea849f23e42664835e2fa8379dcc71
meteorologytoday <meteorologytoday@github.com>
2017-05-12 15:20:19 +0800

    Make month as parameter

--- scripts/routine.sh
+++ scripts/routine.sh
@@ -1,8 +1,13 @@
 #/bin/bash
 scripts_path=$TWSEStockPATH/scripts
 src_path=$TWSEStockPATH/src
-echo "Download Months: 2"
-$scripts_path/download_Stock.sh 12
-$scripts_path/download_BizCorp.sh 12
+months=$1
+if [ -z "$1" ]; then
+	months=2
+fi
+
+echo "Download Months: $months"
+$scripts_path/download_Stock.sh $months
+$scripts_path/download_BizCorp.sh $months
 python3 $src_path/reorder_data.py --database=STOCK.db
 echo "Download is finished!"
